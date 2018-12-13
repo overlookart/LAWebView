17cf12bcca333b9ac02aba5c68967b770321f7c6
xzh <598823426@qq.com>
2018-12-13 20:43:16 +0800

    Update SKCamera.m

--- SKCamera.m
+++ SKCamera.m
@@ -14,18 +14,24 @@
     if (direction) {
         expandX = 0;
         reduceX = 0;
-        expandY = extend;
-        reduceY = -extend;
+        expandY = extent;
+        reduceY = -extent;
     }else{
-        expandX = extend;
-        reduceX = -extend;
+        expandX = extent;
+        reduceX = -extent;
         expandY = 0;
         reduceY = 0;
     }
-    SKAction *expand= [SKAction moveByX:expandX y:expandY duration:0.05];
-    SKAction *reduce = [SKAction moveByX:reduceX y:reduceY duration:0.05];
-    SKAction *group = [SKAction sequence:@[expand,reduce]];
-    SKAction *rep = [SKAction repeatAction:group count:3];
-    [self runAction:rep];
+    SKAction *expand1 = [SKAction moveByX:expandX y:expandY duration:0.05];
+    SKAction *reduce2 = [SKAction moveByX:reduceX y:reduceY duration:0.05];
+    SKAction *expand3 = [SKAction moveByX:expandX/3.0*2 y:expandY/3.0*2 duration:0.05];
+    SKAction *reduce4 = [SKAction moveByX:reduceX/3.0*2 y:reduceY/3.0*2 duration:0.05];
+    SKAction *expand5 = [SKAction moveByX:expandX/3.0 y:expandY/3.0 duration:0.05];
+    SKAction *reduce6 = [SKAction moveByX:reduceX/3.0 y:reduceY/3.0 duration:0.05];
+    SKAction *group = [SKAction sequence:@[expand1,reduce2,expand3,reduce4,expand5,reduce6]];
+    
+//    SKAction *rep = [SKAction repeatAction:group count:3];
+    
+    [self runAction:group];
 }
 @end
